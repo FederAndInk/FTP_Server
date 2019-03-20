2f552091fd08b4088377e36802cc57104c038e0d
FederAndInk <federandink@gmail.com>
2019-03-20 14:39:00 +0100

    add sanitizers, fix receive_line: put \0 at the right place

--- src/ftp_com.c
+++ src/ftp_com.c
@@ -3,7 +3,15 @@
 ssize_t receive_line(rio_t* rp, char* str, size_t maxlen)
 {
   ssize_t n = Rio_readlineb(rp, str, maxlen - 1);
-  str[n - 1] = '\0';
+  if (n > 0)
+  {
+    str[n - 1] = '\0';
+  }
+  else
+  {
+    str[n] = '\0';
+  }
+
   return n;
 }
 
